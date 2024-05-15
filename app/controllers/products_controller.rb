917c9be44654b05c4ca5b8344d36888bfd00b789
laaronchang <aacidus@gmail.com>
2024-05-15 11:03:22 -0700

    made the create code for autogenerating entries

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -9,4 +9,15 @@ class ProductsController < ApplicationController
     @product = Product.find_by(id: params[:id])
     render template:"products/show"
   end
+
+  def create
+    @product = Product.new(
+      item_name: "Newish Item", 
+      item_price: 15, 
+      image_url: "https://i.etsystatic.com/13144413/r/il/924cb7/4470118009/il_fullxfull.4470118009_27d8.jpg", 
+      item_description: "Mon Chichi", 
+    )
+    @product.save
+    render template: "products/show"
+  end
 end
