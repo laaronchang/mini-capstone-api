400fe532bc5091f3a87bbe3317ca3c4db2438257
laaronchang <aacidus@gmail.com>
2024-05-15 11:32:59 -0700

    made params entry, for user to input data; tested with HTTPie in POST and BODY.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -10,12 +10,26 @@ class ProductsController < ApplicationController
     render template:"products/show"
   end
 
+  # This makes a new product entry when using HTTPie and choosing the POST option"
+  # Can verify using the count command or viewing items in rails console ie: Product.count, Product.all
+  # def create
+  #   @product = Product.new(
+  #     item_name: "Newish Item", 
+  #     item_price: 15, 
+  #     image_url: "https://i.etsystatic.com/13144413/r/il/924cb7/4470118009/il_fullxfull.4470118009_27d8.jpg", 
+  #     item_description: "Mon Chichi", 
+  #   )
+  #   @product.save
+  #   render template: "products/show"
+  # end
+
+  # This has the user put their inputs to create a new product entry, in this case using HTTPie
   def create
     @product = Product.new(
-      item_name: "Newish Item", 
-      item_price: 15, 
-      image_url: "https://i.etsystatic.com/13144413/r/il/924cb7/4470118009/il_fullxfull.4470118009_27d8.jpg", 
-      item_description: "Mon Chichi", 
+      item_name: params[:item_name],
+      item_price: params[:item_price], 
+      image_url: params[:image_url], 
+      item_description: params[:item_description], 
     )
     @product.save
     render template: "products/show"
