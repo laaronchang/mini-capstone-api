f3ee84dc185f7e8ee048db73e1ea476617d3f7a7
laaronchang <aacidus@gmail.com>
2024-05-16 10:53:55 -0700

    modified cors file

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,8 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "*"
+    # changed origins to point to website
+    # origins "*"
+    origins "mini-capstone.peterxjang.com"
     resource "*", headers: :any, methods: [:get, :post, :patch, :put, :delete]
   end
 end
