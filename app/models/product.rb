166c251f4398e3ee327a842424b2043425b11d02
laaronchang <aacidus@gmail.com>
2024-05-21 12:15:46 -0700

    added new Supplier  model with entries, added new table to Products model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,5 @@
 class Product < ApplicationRecord
-  validates :item_name, presence: true
+  # validates :item_name, presence: true
 
   def is_discounted?
     p id
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_20_180843) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_21_191426) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,15 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_20_180843) do
     t.decimal "item_price", precision: 12, scale: 2
     t.string "image_url"
     t.string "item_description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "supplier_id"
+  end
+
+  create_table "suppliers", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "phone_number"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
