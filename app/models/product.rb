e8ae63f79bc61b7afd83a3fefdc3a4725bf8364c
laaronchang <aacidus@gmail.com>
2024-06-18 13:24:27 -0700

    add has_many and belongs_to

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,6 +3,8 @@ class Product < ApplicationRecord
 
   has_many :images
   belongs_to :supplier
+  has_many :category_products
+  has_many :categories, through: :category_products
 
   # def is_discounted?
   #   # p id
